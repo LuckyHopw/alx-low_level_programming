6c5ded6f163e57efef52c0b2095ab6acff97074e
LuckyHopw <ogheneruonahope@gmail.com>
2022-07-01 08:55:18 -0700

    programming in c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ * Description: my new line of code 
+ * Return: Always 0 (success) 
+ */
+int main(void){
+	char str1[52];
+	strcpy(str1, "\"programming is like building a multilingual puzzle");	
+	puts(str1);
+	return 0;
+
+}
+
