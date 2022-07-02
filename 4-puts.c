298a7ab7a9219949c090205326659553cc7aca80
LuckyHopw <ogheneruonahope@gmail.com>
2022-07-02 08:13:43 -0700

    hello

--- 4-puts.c
+++ 4-puts.c
@@ -9,10 +9,11 @@ int main(void)
 {
 		char str1[52];
 
-			strcpy(str1, "\"Programming is like building a multilingual puzzle");
+		strcpy(str1, "\"Programming is like building a multilingual puzzle");
 
-				puts(str1);
+		puts(str1);
 
-					return (0);
+		return (0);
 }
+
 
